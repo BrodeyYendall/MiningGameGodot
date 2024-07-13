c5468b8a59780f5bb3a1bc5f0728a594d14a5521
Brodey Yendall <brodey_yendall@hotmail.com>
2024-07-13 22:39:21 +1000

    Vary falling speed and rotation based on cutout size

--- falling_cutout.gd
+++ falling_cutout.gd
@@ -5,6 +5,14 @@ extends Sprite2D
 
 var cutout_vertices: PackedVector2Array
 var viewport_image: Image
+var cutout_size: float
+var adjusted_cutout_size: float
+
+var width: int
+var height: int
+var min_x: int
+var min_y: int
+var rotation_direction: int
 
 func with_data(cutout_vertices: PackedVector2Array, viewport_image: Image) -> Node2D:
 	self.cutout_vertices = cutout_vertices
@@ -14,17 +22,20 @@ func with_data(cutout_vertices: PackedVector2Array, viewport_image: Image) -> No
 func _ready():
 	assert(not cutout_vertices.is_empty(), "cutout created with vertices, please call with_data().")
 	
-	var bounding_box = get_cutout_bounding_box()
-	var x_width = bounding_box[1] - bounding_box[0]
-	var y_width = bounding_box[3] - bounding_box[2]
+	rotation_direction = -1 if randi_range(0, 1) == 0 else 1
+	calculate_cutout_bounding_box()
 	
-	var image = Image.create(x_width, y_width, false, Image.FORMAT_RGBA8)
+	cutout_size = calculate_area()
+	adjusted_cutout_size = cutout_size / 20000
+	adjusted_cutout_size = min(2, max(0.75, adjusted_cutout_size))
+	
+	var image = Image.create(width, height, false, Image.FORMAT_RGBA8)
 	image.set_pixel(0, 0, Color(0, 0, 0, 0)) # Set transparency outside the polygon
 	
-	for y in range(y_width):
-		for x in range(x_width):
-			var world_x = x + bounding_box[0]
-			var world_y = y + bounding_box[2]
+	for y in range(height):
+		for x in range(width):
+			var world_x = x + min_x
+			var world_y = y + min_y
 			var inside_polygon = Geometry2D.is_point_in_polygon(Vector2(world_x, world_y), cutout_vertices)
 			if inside_polygon:
 				var color = viewport_image.get_pixel(world_x, world_y)
@@ -33,36 +44,43 @@ func _ready():
 				image.set_pixel(x, y, Color(0, 0, 0, 0)) # Set transparency outside the polygon
 				
 	texture = ImageTexture.create_from_image(image)
-	position = Vector2(bounding_box[0] + (x_width / 2), bounding_box[2] + (y_width / 2))
+	position = Vector2(min_x + (width / 2), min_y + (height / 2))
 	
 	
-func _process(delta):
-	position.y += fall_speed * delta
-	rotation += rotate_speed * delta
-	if position.y > 1080:
+func _process(delta):	
+	position.y += (fall_speed * delta) / adjusted_cutout_size
+	rotation += (rotate_speed * delta * rotation_direction) / adjusted_cutout_size
+	if position.y > 1080 + height:
 		queue_free()
 	
-func get_cutout_bounding_box() -> Array[int]:
-	var x_min = cutout_vertices[0].x
-	var x_max = cutout_vertices[0].x
-	var y_min = cutout_vertices[0].y
-	var y_max = cutout_vertices[0].y
+func calculate_cutout_bounding_box():
+	min_x = cutout_vertices[0].x
+	min_y = cutout_vertices[0].y
+	
+	var max_x = cutout_vertices[0].x
+	var max_y = cutout_vertices[0].y
 	
 	for vertex in cutout_vertices:
-		if vertex.x < x_min:
-			x_min = vertex.x
+		if vertex.x < min_x:
+			min_x = vertex.x
 		else:
-			x_max = max(x_max, vertex.x)
+			max_x = max(max_x, vertex.x)
 			
-		if vertex.y < y_min:
-			y_min = vertex.y
+		if vertex.y < min_y:
+			min_y = vertex.y
 		else:
-			y_max = max(y_max, vertex.y)
+			max_y = max(max_y, vertex.y)
 			
-	return [x_min, x_max, y_min, y_max]
+	height = max_y - min_y
+	width = max_x - min_x
 	
-func remove_mins(bounding_box: Array[int]):
-	var result = PackedVector2Array()
-	for vertex in cutout_vertices:
-		result.append(Vector2(vertex.x - bounding_box[0], vertex.y - bounding_box[2]))
-	return result
+func calculate_area():
+	var area = 0.0
+	
+	for i in range(cutout_vertices.size()):
+		var j = (i + 1) % cutout_vertices.size()
+		area += cutout_vertices[i].x * cutout_vertices[j].y
+		area -= cutout_vertices[j].x * cutout_vertices[i].y
+	
+	area = abs(area) / 2.0
+	return area
