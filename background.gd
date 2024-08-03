66d175cf10bc881498adef9a02929be0cc74a813
Brodey Yendall <brodey_yendall@hotmail.com>
2024-08-03 12:41:01 +1000

    Changed background generation to basic cellular automata

--- background.gd
+++ background.gd
@@ -10,25 +10,62 @@ func _on_wall_generate_background(seed: int):
 	noise.set_noise_type(FastNoiseLite.TYPE_SIMPLEX_SMOOTH)
 	
 	var window_size = get_viewport().size / 8
+	var map: Array = create_background_matrix(window_size.x, window_size.y)
 
 	for x in range(window_size.x):
 		for y in range(window_size.y):
-			var atlasTarget: Vector2
-			var noiseAtPixel = noise.get_noise_2d(x, y)
-			
-			if noiseAtPixel > rock_level:
-				if noiseAtPixel > deep_rock_level:
-					atlasTarget = Vector2(1, 1)
-				else:
-					atlasTarget = Vector2(1, 0)
+			var rand = randf()
+			if rand >= 0.50:
+				map[y][x] = 1
 			else:
-				if noiseAtPixel > rock_trim_level:
-					atlasTarget = Vector2(0, 0)
-				else:
-					if randi_range(0, 1) == 0:
-						atlasTarget = Vector2(0, 2)
-					else:
-						atlasTarget = Vector2(0, 1)
-					
+				map[y][x] = 0
+			
+	for iteration in range(3):
+		var updated_map = create_background_matrix(window_size.x, window_size.y)
+		for x in range(window_size.x):
+			for y in range(window_size.y):
+				var sum = get_tile_sum(map, x, y, window_size.x, window_size.y)
 				
+				var atlasTarget: Vector2
+				if sum <= 4:
+					updated_map[y][x] = 0
+				else:
+					updated_map[y][x] = 1
+		map = updated_map
+	
+	for x in range(window_size.x):
+		for y in range(window_size.y):
+			var atlasTarget: Vector2
+			if map[y][x] == 0:
+				atlasTarget = Vector2(0, 1)
+			else:
+				atlasTarget = Vector2(1, 0)
 			set_cell(0, Vector2(x, y), 0, atlasTarget)
+	
+func get_tile_sum(map:Array, x: int, y:int, width: int, height: int):
+	var sum = 0
+
+	for i in range(x - 1, x + 2): # + 2 because the upper bound is exclusive
+		if i < 0 or i >= width:
+			sum += 3
+			continue
+		
+		for j in range(y - 1, y + 2):
+				if j < 0 or j >= height:
+					sum += 1
+				else:
+					sum += map[j][i]
+	return sum
+			
+func create_background_matrix(width: int, height: int) -> Array:
+	var height_array = []
+	height_array.resize(height)
+	
+	for i in range(height_array.size()):
+		var width_array = []
+		width_array.resize(width)
+		
+		height_array[i] = width_array
+
+	return height_array
+
