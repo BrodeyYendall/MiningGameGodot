3662639ae54c1185b979cbe7441a92188331ad45
Brodey Yendall <brodey_yendall@hotmail.com>
2025-10-22 13:49:31 +1100

    Seperated wall management from delve management

---
+++ scripts/DelveManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Godot;
+using MiningGame.scripts.helper;
+using MiningGame.scripts.UI;
+
+namespace MiningGame.scripts;
+
+public partial class DelveManager : Node2D
+{
+    [Export] public WallManager WallManager;
+    [Export] public Ui Ui;
+    [Export] public Countdown Countdown;
+    [Export] public GameOverDialogBox GameOverDialogBox;
+
+    public async override void _Ready()
+    {
+        await Restart();
+    }
+
+    public void RestartHandler() { Restart(); }
+    public async Task Restart()
+    {
+        await WallManager.Start();
+        Ui.Restart();
+        GameOverDialogBox.Hide();
+
+        await WallManager.Restart();
+
+        InputManager.Instance.NextWall += WallManager.RemoveFrontWallHandler;
+        InputManager.Instance.CreateHole += WallManager.ProcessCreateHole;
+        
+        Countdown.Start();
+    }
+    
+    public void CountdownCompleted()
+    {
+        InputManager.Instance.NextWall -= WallManager.RemoveFrontWallHandler;
+        InputManager.Instance.CreateHole -= WallManager.ProcessCreateHole;
+        GameOverDialogBox.Show(Ui.GetGoldScore(), Ui.GetZincScore(), WallManager.TopWallNumber); 
+    }
+}
--- scripts/WallManager.cs
+++ scripts/WallManager.cs
@@ -6,38 +6,35 @@ using Godot.Collections;
 using MiningGame.scripts.crack;
 using MiningGame.scripts.cutout;
 using MiningGame.scripts.helper;
-using MiningGame.scripts.UI;
+using MiningGame.scripts.ores;
 
 namespace MiningGame.scripts;
 
 public partial class WallManager : Node2D
 {
-    [Export] public int WallQueueSize = 4;
-    [Export] public int InitialWallsRendered = 2;
-    [Export] public int CutoutEdgeBuffer = 15;
-    [Export] public Node2D FallingCutoutHolder;
-    [Export] public Node2D WallContainer;
-    [Export] public Ui Ui;
-    [Export] public Countdown Countdown;
-    [Export] public GameOverDialogBox GameOverDialogBox;
+    [Signal]
+    public delegate void OreCutoutEventHandler(Ore ore);
     
+    [Export] public int WallQueueSize;
+    [Export] public int InitialWallsRendered;
+    [Export] public int CutoutEdgeBuffer;
+    [Export] public Node2D FallingCutoutHolder;
+    [Export] public Node2D WallHolder;
+
+    public int TopWallNumber => topWallNumber;
     private int topWallNumber = 1;
 
     private readonly List<Wall> activeWalls = [];
     private readonly Queue<Wall> wallQueue = new();
 
-    public async override void _Ready()
-    {
-        await Restart();
-    }
-
-    public void RestartHandler() { Restart(); }
     public async Task Restart()
     {
-        Ui.Restart();
-        GameOverDialogBox.Hide();
-        topWallNumber = 1;
-
+        Clear();
+        await Start();
+    }
+    
+    public void Clear()
+    {
         for (int i = 0; i < activeWalls.Count;)
         {
             activeWalls[i].Destroy();
@@ -48,7 +45,11 @@ public partial class WallManager : Node2D
         {
             wall.Destroy();   
         }
-        
+        topWallNumber = 1;
+    }
+    
+    public async Task Start()
+    {
         for (var i = 0; i < WallQueueSize; i++)
         {
             wallQueue.Enqueue(CreateNewWall(i + 1));
@@ -58,11 +59,6 @@ public partial class WallManager : Node2D
         {
             await ActivateNextWall();
         }
-
-        InputManager.Instance.NextWall += RemoveFrontWallHandler;
-        InputManager.Instance.CreateHole += ProcessCreateHole;
-        
-        Countdown.Start();
     }
 
     private async Task ActivateNextWall()
@@ -74,7 +70,7 @@ public partial class WallManager : Node2D
         wallQueue.Enqueue(CreateNewWall(wallQueue.Peek().WallNumber + 1));
     }
 
-    private void RemoveFrontWallHandler() { RemoveFrontWall(); }
+    public void RemoveFrontWallHandler() { RemoveFrontWall(); }
     private async Task RemoveFrontWall()
     {
         Wall frontWall = activeWalls[0];
@@ -88,13 +84,13 @@ public partial class WallManager : Node2D
     private Wall CreateNewWall(int wallCount)
     {
         Wall wall = Wall.Create(wallCount, FallingCutoutHolder);
-        wall.OreCutout += Ui.ProcessOreCutout;
-        WallContainer.AddChild(wall);
-        WallContainer.MoveChild(wall, 0); // Ensure the wall is at the back.
+        wall.OreCutout += AcceptOreCutoutSignal;
+        WallHolder.AddChild(wall);
+        WallHolder.MoveChild(wall, 0); // Ensure the wall is at the back.
         return wall;
     }
 
-    private void ProcessCreateHole(Vector2 point)
+    public void ProcessCreateHole(Vector2 point)
     {
         Array<Dictionary> raycastResults = RaycastHelper.Instance.RaycastCircle(point, 1, 0xFFFFFFFF, 32);
         int deepestWall = topWallNumber;
@@ -120,12 +116,10 @@ public partial class WallManager : Node2D
         
         activeWalls[targetWall].CreateHole(point);
     }
-
-    public void CountdownCompleted()
+    
+    public void AcceptOreCutoutSignal(Ore ore)
     {
-        InputManager.Instance.NextWall -= RemoveFrontWallHandler;
-        InputManager.Instance.CreateHole -= ProcessCreateHole;
-        GameOverDialogBox.Show(Ui.GetGoldScore(), Ui.GetZincScore(), topWallNumber); 
+        EmitSignalOreCutout(ore);
     }
 
     /// <summary>
